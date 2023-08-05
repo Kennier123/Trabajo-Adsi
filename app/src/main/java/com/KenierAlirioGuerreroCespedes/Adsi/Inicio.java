3d6ca8e93828ae60c8a15ba64d7b2b47a3b30f64
kenier <kenier133@gmail.com>
2023-08-05 11:46:40 -0500

    tercer commit

--- app/src/main/java/com/KenierAlirioGuerreroCespedes/Adsi/Inicio.java
+++ app/src/main/java/com/KenierAlirioGuerreroCespedes/Adsi/Inicio.java
@@ -1,8 +1,13 @@
 package com.KenierAlirioGuerreroCespedes.Adsi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Inicio extends AppCompatActivity {
 
@@ -10,5 +15,37 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        Spinner optionsSpinner = findViewById(R.id.options_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.spinner_options,
+                android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        optionsSpinner.setAdapter(adapter);
+
+        optionsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+
+                // Realiza acciones basadas en la opción seleccionada
+                if (selectedItem.equals("Usuario")) {
+                    // Acción para la opción Usuario
+                } else if (selectedItem.equals("Amigo")) {
+                    // Acción para la opción Amigo
+                } else if (selectedItem.equals("Cerrar Sesión")) {
+                    // Acción para la opción Cerrar Sesión
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // No es necesario implementar nada aquí para este caso
+            }
+        });
     }
 }
