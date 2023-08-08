77dd5d635234f73fd86fe05bfe996ae6a17354ff
kenier <kenier133@gmail.com>
2023-08-08 14:59:16 -0500

    cuarto commit

--- app/src/main/java/com/KenierAlirioGuerreroCespedes/Adsi/Inicio.java
+++ app/src/main/java/com/KenierAlirioGuerreroCespedes/Adsi/Inicio.java
@@ -1,6 +1,8 @@
 package com.KenierAlirioGuerreroCespedes.Adsi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,6 +12,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Inicio extends AppCompatActivity {
+
+    private boolean isFirstSelection = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,17 @@ public class Inicio extends AppCompatActivity {
 
         optionsSpinner.setAdapter(adapter);
 
+        optionsSpinner.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (isFirstSelection) {
+                    optionsSpinner.setSelection(0); // Selecciona el primer elemento
+                    isFirstSelection = false;
+                }
+                return false;
+            }
+        });
+
         optionsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -38,7 +53,9 @@ public class Inicio extends AppCompatActivity {
                 } else if (selectedItem.equals("Amigo")) {
                     // Acción para la opción Amigo
                 } else if (selectedItem.equals("Cerrar Sesión")) {
-                    // Acción para la opción Cerrar Sesión
+                    // Iniciar la actividad Registro
+                    Intent intent = new Intent(Inicio.this, Registro.class);
+                    startActivity(intent);
                 }
             }
 
